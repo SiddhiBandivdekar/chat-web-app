fe73f9e84f27ae060005d55e57f1ca731bd31b0f
Siddhi <bandivdekarsiddhi@gmail.com>
2024-01-09 22:06:01 +0530

    installed firebase

---
+++ src/misc/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyD4BpKk-9PxhLrLDshMgdLQGUgGY-7rcE0',
+  authDomain: 'chat-web-app-c2327.firebaseapp.com',
+  projectId: 'chat-web-app-c2327',
+  storageBucket: 'chat-web-app-c2327.appspot.com',
+  messagingSenderId: '531463206091',
+  appId: '1:531463206091:web:7ff0b96aa42c656405974e',
+};
+
+const app = initializeApp(firebaseConfig);
+
