7cb90d9e0690c29b463fa789f74df89c9489e0f7
Siddhi <bandivdekarsiddhi@gmail.com>
2024-01-10 14:10:05 +0530

    added signin page and signinwithGoogle

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,13 +1,16 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyD4BpKk-9PxhLrLDshMgdLQGUgGY-7rcE0',
-  authDomain: 'chat-web-app-c2327.firebaseapp.com',
-  projectId: 'chat-web-app-c2327',
-  storageBucket: 'chat-web-app-c2327.appspot.com',
-  messagingSenderId: '531463206091',
-  appId: '1:531463206091:web:7ff0b96aa42c656405974e',
+  apiKey: "AIzaSyD4BpKk-9PxhLrLDshMgdLQGUgGY-7rcE0",
+  authDomain: "chat-web-app-c2327.firebaseapp.com",
+  projectId: "chat-web-app-c2327",
+  storageBucket: "chat-web-app-c2327.appspot.com",
+  messagingSenderId: "531463206091",
+  appId: "1:531463206091:web:7ff0b96aa42c656405974e",
 };
 
 const app = initializeApp(firebaseConfig);
-
+export const auth = getAuth(app);
+export const database = getDatabase(app);
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,56 @@
 import React from "react";
+import { Button, Col, Container, Grid, Panel, Row } from "rsuite";
+import GooglePlusCircleIcon from "@rsuite/icons/legacy/GooglePlusCircle";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth, database } from "../misc/firebase";
+import { ref, serverTimestamp, set } from "firebase/database";
 
 const SignIn = () => {
-  return <div>SignIn</div>;
+  const signInProvider = async (provider) => {
+    try {
+      const { user, _tokenResponse } = await signInWithPopup(auth, provider);
+      if (_tokenResponse.isNewUser) {
+        await set(ref(database, `/profiles/${user.uid}`), {
+          name: user.displayName,
+          createdAt: serverTimestamp(),
+        });
+        console.log(user);
+      }
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+  const onGoogleSignUp = () => {
+    signInProvider(new GoogleAuthProvider());
+  };
+
+  return (
+    <Container>
+      <Grid className="mt-page">
+        <Row>
+          <Col xs={24} md={12} mdOffset={6}>
+            <Panel>
+              <div className="text-center">
+                <h2>Welcome to chat</h2>
+                <p>Progressive chat platform</p>
+              </div>
+
+              <div className="mt-3 text-center">
+                <Button
+                  color="blue"
+                  appearance="primary"
+                  startIcon={<GooglePlusCircleIcon />}
+                  onClick={onGoogleSignUp}
+                >
+                  Sign in with Google
+                </Button>
+              </div>
+            </Panel>
+          </Col>
+        </Row>
+      </Grid>
+    </Container>
+  );
 };
 
 export default SignIn;
